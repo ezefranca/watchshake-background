d5098db1488cf6a7b0aac970984e58374b0363d9
ezefranca <ezequiel.ifsp@gmail.com>
2017-05-21 22:08:41 -0300

    initial commit

--- WatchShake-Background WatchKit Extension/InterfaceController.swift
+++ WatchShake-Background WatchKit Extension/InterfaceController.swift
@@ -9,23 +9,101 @@
 import WatchKit
 import Foundation
 
-
-class InterfaceController: WKInterfaceController {
-
+class InterfaceController: WKInterfaceController, WKExtensionDelegate, URLSessionDownloadDelegate {
+    // MARK: Properties
+    
+    let sampleDownloadURL = URL(string: "http://devstreaming.apple.com/videos/wwdc/2015/802mpzd3nzovlygpbg/802/802_designing_for_apple_watch.pdf?dl=1")!
+    
+    @IBOutlet var timeDisplayLabel: WKInterfaceLabel!
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .long
+        
+        return formatter
+    }()
+    
+    // MARK: WKInterfaceController
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        WKExtension.shared().delegate = self
+        updateDateLabel()
     }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    // MARK: WKExtensionDelegate
+    func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
+        for task : WKRefreshBackgroundTask in backgroundTasks {
+            print("received background task: ", task)
+            // only handle these while running in the background
+            if (WKExtension.shared().applicationState == .background) {
+                if task is WKApplicationRefreshBackgroundTask {
+                    // this task is completed below, our app will then suspend while the download session runs
+                    print("application task received, start URL session")
+                    scheduleURLSession()
+                }
+            }
+            else if let urlTask = task as? WKURLSessionRefreshBackgroundTask {
+                let backgroundConfigObject = URLSessionConfiguration.background(withIdentifier: urlTask.sessionIdentifier)
+                let backgroundSession = URLSession(configuration: backgroundConfigObject, delegate: self, delegateQueue: nil)
+                
+                print("Rejoining session ", backgroundSession)
+            }
+            // make sure to complete all tasks, even ones you don't handle
+            task.setTaskCompleted()
+        }
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    // MARK: Snapshot and UI updating
+    
+    func scheduleSnapshot() {
+        // fire now, we're ready
+        let fireDate = Date()
+        WKExtension.shared().scheduleSnapshotRefresh(withPreferredDate: fireDate, userInfo: nil) { error in
+            if (error == nil) {
+                print("successfully scheduled snapshot.  All background work completed.")
+            }
+        }
     }
-
+    
+    func updateDateLabel() {
+        let currentDate = Date()
+        timeDisplayLabel.setText(dateFormatter.string(from: currentDate))
+    }
+    
+    // MARK: URLSession handling
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("NSURLSession finished to url: ", location)
+        updateDateLabel()
+        scheduleSnapshot()
+    }
+    
+    func scheduleURLSession() {
+        let backgroundConfigObject = URLSessionConfiguration.background(withIdentifier: NSUUID().uuidString)
+        backgroundConfigObject.sessionSendsLaunchEvents = true
+        let backgroundSession = URLSession(configuration: backgroundConfigObject)
+        
+        let downloadTask = backgroundSession.downloadTask(with: sampleDownloadURL)
+        downloadTask.resume()
+    }
+    
+    // MARK: IB actions
+    
+    @IBAction func ScheduleRefreshButtonTapped() {
+        // fire in 20 seconds
+        let fireDate = Date(timeIntervalSinceNow: 20.0)
+        // optional, any SecureCoding compliant data can be passed here
+        let userInfo = ["reason" : "background update"] as NSDictionary
+        
+        WKExtension.shared().scheduleBackgroundRefresh(withPreferredDate: fireDate, userInfo: userInfo) { (error) in
+            if (error == nil) {
+                print("successfully scheduled background task, use the crown to send the app to the background and wait for handle:BackgroundTasks to fire.")
+            }
+        }
+    }
+    
 }
